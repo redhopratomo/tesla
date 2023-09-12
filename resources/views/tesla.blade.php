d3841a86d996e9630e83985764fe36f4aa5a2f4f
Redho Pratomo <wakpek176@gmail.com>
2023-09-12 09:30:39 +0700

    first commit

---
+++ resources/views/tesla.blade.php
@@ -0,0 +1,165 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!--mycss-->
+    <link rel="stylesheet" href="style.css"/>
+    <title>tesla | redho</title>
+  </head>
+  <body>
+   <!--Navbar-->
+   <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+    <div class="container">
+      <a class="navbar-brand text-light" href="#">Tesla | Redho</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav ms-auto">
+          <a class="nav-link text-white" href="#Pengertian">Home</a>
+          <a class="nav-link text-white" href="#Tesla">mobil</a>
+          <a class="nav-link text-white" href="#Tesla">Team</a>
+          <a class="nav-link text-white" href="#">Contact Us</a>
+        </div>
+      </div>
+    </div>
+  </nav>
+   <!--akhir navbar-->
+   <!--Hero section-->
+ 
+    <section id="Pengertian" class="about">
+      <div class="container" data-aos="fade-up">
+
+        <div class="row">
+          <div class="col-lg-6 order-1 order-lg-2" data-aos="fade-left" data-aos-delay="100">
+            <img src="img/loho.jpg" class="img-fluid" alt="">
+          </div>
+          <p>
+          <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content" data-aos="fade-right" data-aos-delay="100">
+            <h3 class="text-center bg-danger text-light">Awal berdirinya mobil Tesla  </h3>
+            <p class="fst-italic text-dark">
+                     Tesla Motors didirikan oleh dua insinyur otomotif, yaitu Martin Ebenhard dan Marc Tarpenning pada Juli 2003 di California, Amerika Serikat (AS). Kedua orang ini ingin membuktikan bahwa mobil listrik lebih baik, lebih cepat, dan lebih menyenangkan untuk dikendarai dibanding mobil berbahan bakar minyak. Selain Ebenhard dan Marc Tarpenning, Elon Musk merupakan pendiri sekaligus direktur utama dari Tesla Motors.
+              Nama Tesla sendiri diambil dari nama penemu, fisikawan, teknisi mekanika dan teknisi listrik asal Amerika Serikat, yaitu Nikola Tesla. Berkat penemuan tesla pada abad ke 19 dan awal abad 20, mobil listrik menjadi populer karena pada masa itu, mobil listrik lebih nyaman dan tingkat pengoperasiannya lebih mudah dibanding mobil BBM.
+            </p>
+            <ul class="text-dark">
+              <li><i class="ri-check-double-line"></i> Baterai Yang Lebih Tahan Lama.</li>
+              <li><i class="ri-check-double-line"></i> AI (Artificial Intelligence) Tesla Dibuat Sangat Cerdas.</li>
+              <li><i class="ri-check-double-line"></i> Branding yang Bagus.</li>
+            </ul>
+            <p class="text-dark">
+              Karena keseriusan dalam menggarap mobil listrik tersebut pula, mobil Tesla diklaim sebagai mobil listrik terbaik di dunia dengan berbagai kecanggihan teknologi yang dimiliki.
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </section>
+   <!--ahs-->
+   <section id="features" class="features">
+    <div class="container" data-aos="fade-up">
+
+      <div class="row">
+        <div class="image col-lg-6" style='background-image: url("type s/model\ s\ blck.jpeg");' data-aos="fade-right"></div>
+        <div class="col-lg-6" data-aos="fade-left" data-aos-delay="100">
+          <div class="icon-box mt-5 mt-lg-0" data-aos="zoom-in" data-aos-delay="150">
+            <i class="bx bx-receipt"></i>
+            <h3 class="text-center text-light bg-danger">Spesifikasi Mobil Tesla</h3>
+            <h4 class="text-dark">Sering Update sistem</h4>
+            <p class="text-dark">Seperti halnya software komputer atau aplikasi di smartphone. Tesla membuat sebuah software yang selalu di-update secara berkala untuk menyesuaikan dengan teknologi kekinian.</p>
+          </div>
+          <div class="icon-box mt-5 text-dark" data-aos="zoom-in" data-aos-delay="150">
+            <i class="bx bx-cube-alt"></i>
+            <h4>Desain yang kompleks tapi mewah</h4>
+            <p>Desain tesla yang sederhana tapi mewah yang membuat para milyader ingin mempunyai mobil tsb.</p>
+          </div>
+          <div class="icon-box mt-5 text-dark" data-aos="zoom-in" data-aos-delay="150">
+            <i class="bx bx-images"></i>
+            <h4>Enak dilihat dari eangle manapun</h4>
+            <p>desain tesla yang minimalis membuat mobil ini enak dilihat dari sisi manapun.</p>
+          </div>
+          <div class="icon-box mt-5 text-dark" data-aos="zoom-in" data-aos-delay="150">
+            <i class="bx bx-shield"></i>
+            <h4>Keamanan software yang terjamin</h4>
+            <p>Mobil tesla yang memiliki keamanan software tingkat tinggi, dan tidak ada 1 pun peretas yang bisa meretas sistem tesla.</p>
+          </div>
+        </div>
+        
+      </div>
+
+    </div>
+  </section>
+ 
+   <!--shh-->
+   <!--gssg--
+   <section id="Tesla">
+    <div class="container">
+        <div class="row text-start">
+            <div class="col">
+                <h2>_______________________________________CAR________________________________________</h2>
+            </div>
+        </div>
+        <div class="row grid-item">
+            <div class="col-md-4 mb-3">
+                <div class="card">
+                    <img src="type s/model s wht.jpg" class="card-img-top" alt="Project1">
+                    <div class="card-body">
+                      <p class="card-text text-bold">TESLA MODEL S</p>
+                      <a href="tesla type s.html" class="btn btn-primary">Selengkapnya</a>
+                    </div>
+                  </div>
+            </div>
+            <div class="col-md-4 mb-3">
+                <div class="card">
+                    <img src="type 3/type 3 red.jpeg" class="card-img-top" alt="Project2">
+                    <div class="card-body">
+                      <p class="card-text">TESLA MODEL 3</p>
+                      <a href="#" class="btn btn-primary">Selengkapnya</a>
+                    </div>
+                  </div>
+            </div>  
+            <div class="col-md-4 mb-3">
+                <div class="card">
+                    <img src="type x/type x.1.jpg" class="card-img-top" alt="Project3">
+                    <div class="card-body">
+                      <p class="card-text">TESL MODEL X</p>
+                      <a href="#" class="btn btn-primary">Selengkapnya</a>
+                    </div>
+                  </div>
+                  
+                </div>
+            </div>
+    </div>
+</section>
+  nh-->
+  <section id="About me">
+    <div class="container">
+        <div class="row text-center mb-3">
+            <div class="col">
+            </div>
+        </div>
+        <div class="row  justify-content-center fs-5 text-center text-dark">
+          <h2 class=" bg-danger text-light">Visi dan Misi</h2>
+            <div class="col-4">
+              <h4>Misi</h4>
+                <p>mempercepat munculnya transportasi berkelanjutan dengan membawa mobil listrik pasar massal yang menarik ke pasar sesegera mungkin</p>
+            </div>
+            <div class="col-4">
+              <h3>Visi</h3>
+              <p>"mengembangkan perusahaan mobil abad kedua puluh satu yang menarik dengan mendorong transisi dunia ke kendaraan listrik.</p>
+            </div>
+        </div>
+    </div>
+  </section>
+    
+   <!--Akhir hero-->
+   <!--team-->
+
+   <!--Akhr-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  </body>
+</html>
